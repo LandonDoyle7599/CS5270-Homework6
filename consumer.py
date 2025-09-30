06c2402622e4ac93487dec3f4ab8931e53e27111
LandonDoyle7599 <landondoyle7599@gmail.com>
2025-09-30 00:29:17 -0600

    copied arguments from provided consumer and used arg parse

--- consumer.py
+++ consumer.py
@@ -1,9 +1,10 @@
 import boto3
+import argparse
 
 class Consumer:
     def __init__(self):
-        return
-    
+        self.args = self.parse_arguments()
+
     def process_widgets(self):
         return
     
@@ -12,9 +13,44 @@ class Consumer:
     
     def store_in_s3(self):
         return
+    
+    def parse_arguments(self):
+        arg_parser = argparse.ArgumentParser(description='Consumer for processing widget requests.')
+        arg_parser.add_argument('-p', '--profile', type=str, default='default', help='Name of AWS profile to use for credentials (default=default)')
+        arg_parser.add_argument('-r', '--region', type=str, default='us-east-1', help='AWS region to use (default=us-east-1)')
+        arg_parser.add_argument('-mrt', '--max-runtime', type=int, default=0, help='Maximum runtime in milliseconds, with 0 meaning no maximum (default=0)')
+        arg_parser.add_argument('-rb', '--request-bucket', type=str, required=True, help='Name of bucket that will contain requests')
+        arg_parser.add_argument('-uop', '--use-owner-in-prefix', type=bool, default=False, help="Use the owner's name in the object's prefix when storing in S3")
+        arg_parser.add_argument('-rq', '--request-queue', type=str,  help='URL of queue that will contain requests')
+        arg_parser.add_argument('-wb', '--widget-bucket', type=str, help='Name of the S3 bucket that holds the widgets')
+        arg_parser.add_argument('-wkp', '--widget-key-prefix', type=str, default='widgets/', help='Prefix for widget objects (default=widgets/)')
+        arg_parser.add_argument('-dwt', '--dynamodb-widget-table', type=str,  help='Name of the DynamoDB table that holds widgets')
+        arg_parser.add_argument('-pdbc', '--pdb-conn', type=str, help='Postgres Database connection string')
+        arg_parser.add_argument('-pdbu', '--pdb-username', type=str, help='Postgres Database username')
+        arg_parser.add_argument('-pdbp', '--pdb-password', type=str, help='Password for the Postgres database user')
+        arg_parser.add_argument('-qwt', '--queue-wait-timeout', type=int, default=10, help='The duration (in seconds) to wait for a message to arrive in the request when trying to receive a message (default=10)')
+        arg_parser.add_argument('-qvt', '--queue-visibility-timeout', type=int, default=2, help='The duration (in seconds) to the messages received from the queue are hidden from others (default=2)')
+        return arg_parser.parse_args()
 
 if __name__ == '__main__':
     consumer = Consumer()
 
 
 
+
+
+#arguments from provided consumer
+#         -p, --profile                  Name of AWS profile to use for credentials (default=default)
+#         -r, --region                   Name of AWS profile to use for credentials (default=us-east-1)
+#         -mrt, --max-runtime            Maximum runtime in milliseconds, with 0 meaning no maximum (default=30000 for producers, 0 for consumers)
+#         -rb, --request-bucket          Name of bucket that will contain requests (default=null
+#         -uop, --use-owner-in-prefix    User the owner's name in the object's prefix when storing in S3
+#         -rq, --request-queue           URL of queue that will contain requests (default=null
+#         -wb, --widget-bucket           Name of the S3 bucket that holds the widgets (default=null)
+#         -wkp, --widget-key-prefix      Prefix for widget objects (default=widgets/)
+#         -dwt, --dynamodb-widget-table  Name of the DynamoDB table that holds widgets (default=null)
+#         -pdbc, --pdb-conn              Postgres Database connection string
+#         -pdbu, --pdb-username          Postgres Database username
+#         -pdbp, --pdb-password          Password for the Postgres database user
+#         -qwt, --queue-wait-timeout     The duration (in seconds) to wait for a message to arrive in the request when trying to receive a message (default=10)
+#         -qvt, --queue-visibility-timeout The duration (in seconds) to the messages received from the queue are hidden from others (default=2)
