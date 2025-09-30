c1f89441a18be60f5e8cc5ab8ef19c8f7b643c52
LandonDoyle7599 <landondoyle7599@gmail.com>
2025-09-30 00:22:55 -0600

    initial class and git ignore

---
+++ consumer.py
@@ -0,0 +1,20 @@
+import boto3
+
+class Consumer:
+    def __init__(self):
+        return
+    
+    def process_widgets(self):
+        return
+    
+    def store_in_dynamo(self):
+        return
+    
+    def store_in_s3(self):
+        return
+
+if __name__ == '__main__':
+    consumer = Consumer()
+
+
+
