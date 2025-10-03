616534320594d8570324daac94af0ecd9f8098f8
LandonDoyle7599 <landondoyle7599@gmail.com>
2025-10-02 20:24:29 -0600

    created test file and commented in needed tests

---
+++ test_consumer.py
@@ -0,0 +1,33 @@
+import pytest
+
+
+
+#test initialization
+
+
+#test widget processing logic
+
+
+
+#test check s3 (request bucket) empty function
+
+
+
+#test retrieve s3 request function
+
+
+
+#test widget create function
+
+
+#test initialize logger function
+
+
+#test parse arguments function
+
+
+#TODO: Test widget delete function
+
+#TODO: Test widget update function
+
+
